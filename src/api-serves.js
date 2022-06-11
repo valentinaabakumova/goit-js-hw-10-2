bf0adc42a654a71e3e13315e7684df64a2efc84b
valentinaabakumova <abakumova.va13@gmail.com>
2022-06-11 12:11:56 +0200

    1 not

--- src/api-serves.js
+++ src/api-serves.js
@@ -1,10 +1,9 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
-export function fetchCountries(name){
-     return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json()}
-)}
-  
+const BASE_URL = 'https://pokeapi.co/api/v2';
 
+function fetchPokemon(pokemonId) {
+  return fetch(`${BASE_URL}/pokemon/${pokemonId}`).then(response =>
+    response.json()
+  );
+}
+
+export default { fetchPokemon };
--- src/refs.js
+++ src/refs.js
@@ -1,10 +1,6 @@
 export default function getRefs() {
   return {
-    // inputCountry: document.querySelector('#search-box'),
-    // countryInfoContainer: document.querySelector('.country-info'),
-
-    inputCountry: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-list'),
-    countryInfoContainer: document.querySelector('.country-info'),
+    cardContainer: document.querySelector('.js-card-container'),
+    searchForm: document.querySelector('.js-search-form'),
   };
 }
